77e5d3f562c299825f1b76f16ed44ad3594511a8
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-12-12 13:51:42 -0800

    Match table data structure

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,4 +1,7 @@
 import {AstraDB} from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+
+
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 import 'dotenv/config'
 import {CohereClient} from "cohere-ai";
@@ -11,11 +14,7 @@ const cohere = new CohereClient({
 });
 
 const taylorData = [
-  {
-    url: '',
-    title: '',
-    content: '',
-  }
+  'https://time.com/6342806/person-of-the-year-2023-taylor-swift/',
 ];
 
 const {ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_ID, ASTRA_DB_REGION, ASTRA_DB_NAMESPACE} = process.env;
@@ -38,7 +37,8 @@ const createCollection = async (similarityMetric: SimilarityMetric = 'dot_produc
 
 const loadSampleData = async (similarityMetric: SimilarityMetric = 'dot_product') => {
   const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
-  for await (const {url, title, content} of taylorData) {
+  for await (const url of taylorData) {
+    const content = await scrapePage(url);
     const chunks = await splitter.splitText(content);
     let i = 0;
     for await (const chunk of chunks) {
@@ -49,15 +49,29 @@ const loadSampleData = async (similarityMetric: SimilarityMetric = 'dot_product'
       });
 
       const res = await collection.insertOne({
-        document_id: `${url}-${i}`,
         $vector: embedded[0]?.embedding,
-        url,
-        title,
-        content: chunk
+        text: chunk
       });
       i++;
     }
   }
+};
+
+const scrapePage = async (url: string) => {
+  const loader = new PuppeteerWebBaseLoader(url, {
+    launchOptions: {
+      headless: "new"
+    },
+    gotoOptions: {
+      waitUntil: "domcontentloaded",
+    },
+    evaluate: async (page, browser) => {
+      const result = await page.evaluate(() => document.body.innerHTML);
+      await browser.close();
+      return result;
+    },
+  });
+  return (await loader.scrape())?.replace(/<[^>]*>?/gm, '');
 };
 
 createCollection().then(() => loadSampleData());
