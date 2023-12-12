7c4e827b66f33a1c4f22e6f8c76284a2b75e205f
phact <estevezsebastian@gmail.com>
2023-12-11 23:04:26 -0500

    v1

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -21,7 +21,7 @@ const cohere = new CohereClient({
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
   // baseURL: "https://open-assistant-ai.astra.datastax.com/v1",
-  baseURL: "https://2ed8-45-37-48-83.ngrok-free.app",
+  baseURL: "https://2ed8-45-37-48-83.ngrok-free.app/v1",
   defaultHeaders: {
     "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
   }
