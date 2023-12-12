e1e9fdb7ba07062299d99baf4f21a346db51a3ba
phact <estevezsebastian@gmail.com>
2023-12-12 01:55:07 -0500

    key

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -19,13 +19,11 @@ const cohere = new CohereClient({
 });
 
 const openai = new OpenAI({
-  apiKey: "none",
+  apiKey: OPENAI_API_KEY,
   baseURL: "https://open-assistant-ai.astra.datastax.com/v1",
   // baseURL: "https://2ed8-45-37-48-83.ngrok-free.app/v1",
   defaultHeaders: {
     "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
-    "api-key": COHERE_API_KEY,
-    "embedding-model": "cohere/embed-english-v3.0",
   }
 });
 
